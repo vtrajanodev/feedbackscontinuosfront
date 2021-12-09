d39e3a8750ce48b398a044f1aaadc79194db04c1
hadnaNOlii <nanda.oliveira.pereira@gmail.com>
2021-12-09 19:53:59 -0300

    Criando a tela de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,31 @@
-import styles from './Login.module.css';
+import { Formik, Field, Form } from 'formik';
+// import { useContext } from 'react';
+// import { AuthContext } from '../context/AuthContext';
+import styles from './login.module.css';
 
 export const Login = () => {
   return (
-    <>
-    </>
-  )
+    <div className={styles.content}>
+    <h1>Login</h1>
+    <Formik
+      initialValues={{
+        user: '',
+        password: '',
+      }}
+      onSubmit={async (values) => {
+        
+      }}
+    >
+      <Form>
+        <label htmlFor="user">Usuário: </label>
+        <Field id="user" name="user" placeholder="Digite seu usuário" />
+
+        <label htmlFor="password">Senha: </label>
+        <Field id="password" name="password" placeholder="*******" />
+
+        <button type="submit">Login</button>
+      </Form>
+    </Formik>
+  </div>
+  );
 }
