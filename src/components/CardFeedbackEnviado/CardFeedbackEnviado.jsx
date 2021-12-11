3a778854d6dacecf0b7f7fa37a66fbabe8215429
Victor Trajano <trajanodev@hotmail.com>
2021-12-11 17:56:03 -0400

    Atualização de envio de feedback

--- src/components/CardFeedbackEnviado/CardFeedbackEnviado.jsx
+++ src/components/CardFeedbackEnviado/CardFeedbackEnviado.jsx
@@ -1,3 +1,5 @@
+import { format, compareAsc, parseISO } from 'date-fns'
+
 
 export const CardFeedbackEnviado = ({ styles, feedbacksEnviados, logo }) => {
   return (
---
+++ src/pages/SendFeedback/SendFeedback.jsx
@@ -0,0 +1,9 @@
+import styles from './sendfeedback.module.css'
+
+export const SendFeedback = () => {
+  return (
+    <div>
+      <h1>Hello feedback</h1>
+    </div>
+  )
+}
