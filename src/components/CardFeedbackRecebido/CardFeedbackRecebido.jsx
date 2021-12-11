a836f9d85519e9fd9ff5468bc8e710fe6afc4e35
Victor Trajano <trajanodev@hotmail.com>
2021-12-11 13:35:12 -0400

    Atualização dos cards da pagina incial

--- src/components/CardFeedbackRecebido/CardFeedbackRecebido.jsx
+++ src/components/CardFeedbackRecebido/CardFeedbackRecebido.jsx
@@ -10,9 +10,15 @@ export const CardFeedbackRecebido = ({ styles, feedbacksRecebidos, logo }) => {
               <div>
                 <img src={logo} alt="Imagem perfil card" />
               </div>
-              <div className={styles.cardContent}>
-                <h3>{feedback.funcionarioOrigem.nome}</h3>
-                <p>{feedback.conteudo}</p>
+              <div>
+                <div className={styles.cardTitle}>
+                  <h3>{feedback.funcionarioOrigem.nome}</h3>
+                  <span>{feedback.dataFeedback}</span>
+                </div>
+                <div className={styles.cardContent}>
+                  <p>{feedback.conteudo}</p>
+                  <button>Tornar invisivel</button>
+                </div>
               </div>
             </div>
           </div>
