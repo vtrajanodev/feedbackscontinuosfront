728a6c64d7b9b85358b295df4f8454a6bc8e9806
Victor Trajano <trajanodev@hotmail.com>
2021-12-12 18:11:57 -0400

    Criação do modal de envio de feedback

--- src/components/CardSendFeedback/CardSendFeedback.jsx
+++ src/components/CardSendFeedback/CardSendFeedback.jsx
@@ -1,32 +1,53 @@
 import { useContext } from "react"
+import { useState } from "react"
 import { EmployeeContext } from "../../context/EmployeeContext"
+import Modal from 'react-modal'
 import logo from '../../images/img.jpg'
+
+Modal.setAppElement('#root')
 
 export const CardSendFeedback = ({ styles }) => {
 
   const { employeeList } = useContext(EmployeeContext)
 
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleOpenNewSendFeedbackModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleCloseNewSendFeedbackModal = () => {
+    setIsModalOpen(false)
+
+  }
+
   return (
     <div className={styles.cardList}>
-    <h1>Enviar feedback</h1>
-    {employeeList.map(employee => (
-      <div key={employee.idFuncionario}>
-        <div className={styles.card}>
-          <div>
-            <img src={logo} alt="Imagem perfil card" />
-          </div>
-          <div className={styles.cardContent}>
+      <h1>Enviar feedback</h1>
+      {employeeList.map(employee => (
+        <div key={employee.idFuncionario}>
+          <div className={styles.card}>
             <div>
-              <h3>{employee.nome}</h3>
-              <p>{employee.email}</p>
+              <img src={logo} alt="Imagem perfil card" />
             </div>
-            <div>
-              <button>Enviar um feedback</button>
+            <div className={styles.cardContent}>
+              <div>
+                <h3>{employee.nome}</h3>
+                <p>{employee.email}</p>
+              </div>
+              <div>
+                <button onClick={handleOpenNewSendFeedbackModal} >Enviar um feedback</button>
+                <Modal
+                  isOpen={isModalOpen}
+                  onRequestClose={handleCloseNewSendFeedbackModal}
+                >
+                  <h2>Oi sou um modal</h2>
+                </Modal>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    ))}
-  </div>
+      ))}
+    </div>
   )
 }
