de0e324a593e912a231a460a77fe8d80199ae136
Victor Trajano <trajanodev@hotmail.com>
2021-12-13 01:19:33 -0400

    Estilização do modal de envio de feedback

--- src/components/CardSendFeedback/CardSendFeedback.jsx
+++ src/components/CardSendFeedback/CardSendFeedback.jsx
@@ -37,36 +37,46 @@ export const CardSendFeedback = ({ styles }) => {
               </div>
               <div>
                 <button onClick={handleOpenNewSendFeedbackModal} >Enviar um feedback</button>
+
                 <Modal
                   isOpen={isModalOpen}
                   onRequestClose={handleCloseNewSendFeedbackModal}
+                  overlayClassName={styles.reactModalOverlay}
+                  className={styles.reactModalContent}
                 >
-                  <h3>Envie o seu feedback para nomeDoUsuario</h3>
+                  <h4>Envie o seu feedback para nomeDoUsuario</h4>
 
-                  <div className={styles.feedbackInfo}>
-                    Envie elogios ou criticas construtivas que possam auxiliar na evolução do desempenho de seus colegas.
-                  </div>
-                  <div className={styles.feedbackContent}>
-                    <p>Selecione o tipo do seu feedback</p>
+                  <form>
+                    <div className={styles.feedbackInfo}>
+                      <p>
+                        Envie elogios ou criticas construtivas que possam auxiliar na evolução do desempenho de seus colegas.
+                      </p>
+                    </div>
+                    <div className={styles.feedbackContent}>
+                      <p>Selecione o tipo do seu feedback</p>
 
-                    <div className={styles.feedbackType}>
-                      <span>Icone</span>
-                      <span>Icone</span>
+                      <div className={styles.feedbackType}>
+                        <span>Icone</span>
+                        <span>Icone</span>
+                      </div>
+
+                      <div>
+                        <label htmlFor="feedbackTags">Selecione uma tag: </label> <br />
+                        <select name="feedbackTags" id="feedbackTags">
+                          <option value="tag1">Tag1</option>
+                          <option value="tag2">Tag2</option>
+                          <option value="tag3">Tag3</option>
+                          <option value="tag4">Tag4</option>
+                        </select>
+                      </div>
+
+                      <div>
+                        <textarea name="feedbackTextArea" id="feedbackTextArea"></textarea>
+                      </div>
+                      <button type="submit">Enviar</button>
                     </div>
 
-                    <div className={styles.feedbackTags}>
-                      <select name="feedbackTags" id="feedbackTags">
-                        <option value="tag1">Tag1</option>
-                        <option value="tag2">Tag2</option>
-                        <option value="tag3">Tag3</option>
-                        <option value="tag4">Tag4</option>
-                      </select>
-                    </div>
-
-                    <div>
-                      <textarea name="feedbackTextArea" id="feedbackTextArea" cols="100" rows="5"></textarea>
-                    </div>
-                  </div>
+                  </form>
                 </Modal>
               </div>
             </div>
