feddce08a05068e51ed9be3480bfaebdbcc317ce
hadnaNOlii <nanda.oliveira.pereira@gmail.com>
2021-12-20 01:53:03 -0300

    Layout Login conluido

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -46,7 +46,7 @@ export const Login = () => {
                 <button type="submit">Login</button>
               </div>
                <small>
-               <Link to="/cadastro-usuario">Não possuo cadastro</Link>
+               <Link to="/cadastro-usuario">Não possui cadastro? Clique aqui</Link>
                </small> 
             </div>
           </div>
